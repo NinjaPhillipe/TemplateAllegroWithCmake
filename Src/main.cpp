041b06047f98bb41465403a1595e53a8243d0513
NinjaPhillipe <23127972+NinjaPhillipe@users.noreply.github.com>
2019-12-13 13:25:00 +0100

    first commit

---
+++ Src/main.cpp
@@ -0,0 +1,120 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+
+
+
+void must_init(bool test, const char *description)
+{
+    if(test) return;
+
+    printf("couldn't initialize %s\n", description);
+    exit(1);
+}
+
+
+int main(int argc, char **argv){
+  // srand(time(NULL)); // initialisation random
+
+  must_init(al_init(), "allegro");
+  must_init(al_install_keyboard(), "keyboard");
+  must_init(al_init_primitives_addon(), "primitives");
+
+  ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+  must_init(timer, "timer");
+
+  ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+  must_init(queue, "queue");
+
+  // antialiasing
+  al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
+  al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
+  al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR);
+
+  ALLEGRO_DISPLAY* disp = al_create_display(640, 480);
+  must_init(disp, "display");
+
+  ALLEGRO_FONT* font = al_create_builtin_font();
+  must_init(font, "font");
+
+
+
+  al_register_event_source(queue, al_get_keyboard_event_source());
+  al_register_event_source(queue, al_get_display_event_source(disp));
+  al_register_event_source(queue, al_get_timer_event_source(timer));
+
+  bool done = false;
+  bool redraw = true;
+  ALLEGRO_EVENT event;
+
+    // INIT KEY
+  #define KEY_SEEN     1
+  #define KEY_RELEASED 2
+  unsigned char key[ALLEGRO_KEY_MAX];
+  memset(key, 0, sizeof(key));
+
+  al_start_timer(timer);
+  while(!done)
+  {
+    al_wait_for_event(queue, &event);
+
+        switch(event.type)
+    {
+      case ALLEGRO_EVENT_TIMER:
+
+        if(key[ALLEGRO_KEY_UP])
+          //Do things
+        if(key[ALLEGRO_KEY_DOWN])
+           //Do things
+        if(key[ALLEGRO_KEY_LEFT])
+           //Do things
+        if(key[ALLEGRO_KEY_RIGHT])
+           //Do things
+        if(key[ALLEGRO_KEY_ESCAPE])
+          done = true;
+
+        for(int i = 0; i < ALLEGRO_KEY_MAX; i++)
+          key[i] &= KEY_SEEN;
+
+        redraw = true;
+        break;
+      case ALLEGRO_EVENT_KEY_DOWN:
+        key[event.keyboard.keycode] = KEY_SEEN | KEY_RELEASED;
+        break;
+      case ALLEGRO_EVENT_KEY_UP:
+        key[event.keyboard.keycode] &= KEY_RELEASED;
+        break;
+
+
+
+
+      case ALLEGRO_EVENT_DISPLAY_CLOSE:
+        done = true;
+        break;
+    }
+
+
+    if(redraw && al_is_event_queue_empty(queue))
+    {
+      al_clear_to_color(al_map_rgb(0, 0, 0));\
+
+      //draw
+
+      al_flip_display();
+
+      redraw = false;
+    }
+  }
+
+
+  al_destroy_font(font);
+  al_destroy_display(disp);
+  al_destroy_timer(timer);
+  al_destroy_event_queue(queue);
+
+  return 0;
+}
